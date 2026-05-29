06bd44e3cae442a7a7459c500c728207c736f9bb
Thanh Manh <manhthanhta2004@gmail.com>
2026-05-29 16:44:36 +0700

    Them asset và hiệu ứng

---
+++ Assets/Manh/Script/Game/Bomb.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [Header("Explosion Effect")]
+    public GameObject explosionEffect;
+
+    [Header("Explosion Delay")]
+    public float explodeDelay = 1f;
+
+    [Header("Bomb Object")]
+    public GameObject bombVisual;
+
+    public bool hasExploded = false;
+
+
+    // =========================
+    // BẬT NỔ
+    // =========================
+
+    public void TriggerBomb()
+    {
+        // CHỐNG NỔ NHIỀU LẦN
+        if (hasExploded)
+            return;
+
+        hasExploded = true;
+
+        StartCoroutine(
+            ExplosionRoutine()
+        );
+    }
+
+    IEnumerator ExplosionRoutine()
+    {
+        // CHỜ TRƯỚC KHI NỔ
+        yield return new WaitForSeconds(
+            explodeDelay
+        );
+
+        // EFFECT
+        GameObject ef = null;
+
+        // =========================
+        // HIỆU ỨNG NỔ
+        // =========================
+
+        if (explosionEffect != null)
+        {
+            ef = Instantiate(
+                explosionEffect,
+                transform.position,
+                Quaternion.identity
+            );
+        }
+
+        // =========================
+        // TẮT MODEL BOOM
+        // =========================
+
+        if (bombVisual != null)
+        {
+            bombVisual.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+
+        // =========================
+        // XÓA EFFECT
+        // =========================
+
+        if (ef != null)
+        {
+            Destroy(ef, 1.2f);
+        }
+    }
+}
---
+++ Assets/Manh/Script/Game/FloatingObject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class FloatingObject : MonoBehaviour
+{
+    [Header("Float Setting")]
+    public float floatHeight = 0.3f;
+    public float floatSpeed = 2f;
+
+    [Header("Rotate Setting")]
+    public float rotateSpeed = 20f;
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        // Lưu vị trí ban đầu
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // =========================
+        // LƯ LỬNG
+        // =========================
+
+        float newY =
+            startPos.y +
+            Mathf.Sin(
+                Time.time * floatSpeed
+            ) * floatHeight;
+
+        transform.position =
+            new Vector3(
+                startPos.x,
+                newY,
+                startPos.z
+            );
+
+        // =========================
+        // XOAY NHẸ
+        // =========================
+
+        transform.Rotate(
+            Vector3.up *
+            rotateSpeed *
+            Time.deltaTime
+        );
+    }
+}
