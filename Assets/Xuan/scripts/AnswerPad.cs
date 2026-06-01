616be6f3a6c67de3f4de7a3be2c6deeab0c013d7
Xuanharu <xuan12345.912.pch@gmail.com>
2026-06-01 15:26:58 +0700

    minigame3

---
+++ Assets/Xuan/scripts/AnswerPad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AnswerPad : MonoBehaviour
+{
+    public MathManager mathManager;
+    public int padIndex; // 0=A, 1=B, 2=C, 3=D
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra xem đối tượng va chạm có gắn script PlayerType của bạn không
+        PlayerType playerType = other.GetComponent<PlayerType>();
+
+        if (playerType != null)
+        {
+            // Truyền thông tin: là Player 2 (true) hay Player 1 (false), kèm theo chỉ số ô dậm
+            mathManager.OnPlayerStepOnPad(playerType.isPlayer2, padIndex);
+        }
+    }
+}
