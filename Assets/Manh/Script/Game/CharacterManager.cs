abf2fac92e5fb190e83494da8ac0b4593fc539bc
Thanh Manh <manhthanhta2004@gmail.com>
2026-06-16 15:00:46 +0700

    Update

--- Assets/Manh/Script/Game/CharacterManager.cs
+++ Assets/Manh/Script/Game/CharacterManager.cs
@@ -22,8 +22,9 @@ public class CharacterManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        indexPlayer1 = SendIndexCharacter.Instance.player1Index;
-        indexPlayer2 = SendIndexCharacter.Instance.player2Index;
+        
+      //  indexPlayer1 = SendIndexCharacter.Instance.player1Index;
+       // indexPlayer2 = SendIndexCharacter.Instance.player2Index;
         SetUpPlayer();
         
     }
