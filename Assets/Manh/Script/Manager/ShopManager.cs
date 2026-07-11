967809d73ec7b3dfa1fb4595bce6bed9fde0c2f9
Thanh Manh <manhthanhta2004@gmail.com>
2026-07-11 14:18:56 +0700

    Update

--- Assets/Manh/Script/Manager/ShopManager.cs
+++ Assets/Manh/Script/Manager/ShopManager.cs
@@ -79,10 +79,7 @@ public class ShopManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    public void Start()
-    {
-        Open();
-    }
+   
     #endregion
 
     // =========================================================
