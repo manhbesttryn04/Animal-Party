d847701da611108e18e221f9e1c80b6b5518b692
Thanh Manh <manhthanhta2004@gmail.com>
2026-08-23 12:59:24 +0700

    Update

--- Assets/Main/Script/Manager/SettingManager.cs
+++ Assets/Main/Script/Manager/SettingManager.cs
@@ -2209,7 +2209,6 @@ public class SettingManager : MonoBehaviour
                 UIManager.Instance.openSettingPanelButton.SetActive(
                     false
                 );
-                UIManager.Instance.isShowKeyBoard = false;
             }
 
             if (UIManager.Instance.canvasNotifi != null)
